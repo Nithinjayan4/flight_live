51f1c7f7c60a494abfb9582f8371b34d491e3981
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-09-16 02:35:08 +0530

    depatures flight live completed

--- Server.js
+++ Server.js
@@ -1,18 +1,33 @@
 const PORT =8000
+const axios = require("axios").default
+const express =require('express')
+const app = express()
+const cors =require('cors')
+require('dotenv').config()
+app.use(cors())
+app.get('/flights',(req,res)=>{
 
-const axios = require("axios");
+    const options = {
+        method: 'GET',
+        url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
+        headers: {
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
+        }
+    };
 
-const options = {
-    method: 'GET',
-    url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
-    headers: {
-        'X-RapidAPI-Key': 'a92b61cd37mshb12828258623a2fp1d1c47jsnaab6cd32fae6',
-        'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
-    }
-};
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        res.json(response.data.slice(0,6))
+    }).catch(function (error) {
+        console.error(error);
+    })
 
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});
+
+})
+
+
+
+
+
+app.listen(PORT,()=>console.log('running on PORT' + PORT))
